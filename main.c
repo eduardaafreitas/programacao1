ca7c8d003d24ddd97ac03e6a8c09d2f1e03751b5
Luize Duarte <82662556+luizeduarte@users.noreply.github.com>
2023-02-08 14:05:48 -0300

    Atualizar o main.c
    criação do while.

--- main.c
+++ main.c
@@ -5,37 +5,43 @@
 
 
 int main(){
-	int pontos, pedidos_errados, uso_lixeira, num_clientes = 0; 
+	int pontos, pedidos_errados, uso_lixeira, num_clientes, num_pedidos_certos = 0; 
 	struct locais *elementos_mapa;
 	struct refeicao* topo = NULL;
 	struct pedido* comeco = NULL;
 	struct pedido* fim = NULL;
-
+	inicializa_fila(comeco, fim, &num_clientes);
 	inicializa_ncurses();
 	int movimento = getch();
 
-	switch (movimento){
-		case KEY_RIGHT:{
-			if (move_direita(topo, comeco, &pontos, &pedidos_errados, &uso_lixeira, elementos_mapa) == 0)
-				mvaddch(elementos_mapa->chapeiro.lin, elementos_mapa->chapeiro.col + 1, elementos_mapa->chapeiro.simbolo);
-		} break;
-
-		case KEY_LEFT:{
-			if (move_esquerda(topo, comeco, &pontos, &pedidos_errados, &uso_lixeira, elementos_mapa) == 0)
-				mvaddch(elementos_mapa->chapeiro.lin, elementos_mapa->chapeiro.col - 1, elementos_mapa->chapeiro.simbolo);
-		} break;
-
-		case KEY_DOWN:{
-			if (move_baixo(topo, comeco, &pontos, &pedidos_errados, &uso_lixeira, elementos_mapa) == 0)
-				mvaddch(elementos_mapa->chapeiro.lin + 1, elementos_mapa->chapeiro.col, elementos_mapa->chapeiro.simbolo);
-		} break;
-
-		case KEY_UP:{
-			if (move_cima(topo, comeco, &pontos, &pedidos_errados, &uso_lixeira, elementos_mapa) == 0)
-				mvaddch(elementos_mapa->chapeiro.lin - 1, elementos_mapa->chapeiro.col, elementos_mapa->chapeiro.simbolo);
-		} break;
+	while (num_pedidos_certos < 8){
+		switch (movimento){
+			case KEY_RIGHT:{
+				if (move_direita(topo, comeco, &pontos, &pedidos_errados, &uso_lixeira, elementos_mapa) == 0)
+					mvaddch(elementos_mapa->chapeiro.lin, elementos_mapa->chapeiro.col + 1, elementos_mapa->chapeiro.simbolo);
+			} break;
+	
+			case KEY_LEFT:{
+				if (move_esquerda(topo, comeco, &pontos, &pedidos_errados, &uso_lixeira, elementos_mapa) == 0)
+					mvaddch(elementos_mapa->chapeiro.lin, elementos_mapa->chapeiro.col - 1, elementos_mapa->chapeiro.simbolo);
+			} break;
+	
+			case KEY_DOWN:{
+				if (move_baixo(topo, comeco, &pontos, &pedidos_errados, &uso_lixeira, elementos_mapa) == 0)
+					mvaddch(elementos_mapa->chapeiro.lin + 1, elementos_mapa->chapeiro.col, elementos_mapa->chapeiro.simbolo);
+			} break;
+	
+			case KEY_UP:{
+				if (move_cima(topo, comeco, &pontos, &pedidos_errados, &uso_lixeira, elementos_mapa) == 0)
+					mvaddch(elementos_mapa->chapeiro.lin - 1, elementos_mapa->chapeiro.col, elementos_mapa->chapeiro.simbolo);
+			} break;
+		}
+		
+		if ((num_clientes < 7) && (rand()%2 == 1)){
+				insere_fim(comeco, fim, &num_clientes);
+				num_clientes++;
+			}
 	}
-
 	endwin();
 	return 0;
 }
